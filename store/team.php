ed48bae8f928194062c4c20eb2b6ac70b6ca8c97
Gema Aji Wardian <gema_wardian@hotmail.com>
2016-08-22 14:12:04 +0700

    Updated app info and license.

--- store/team.php
+++ store/team.php
@@ -6,5 +6,33 @@
 <body>
 Programmed by : <a href="mailto:gema_wardian@hotmail.com">Gema Aji W.</a> and <a href="mailto:dangridho99@gmail.com">Dang Ridho</a><br>
 Designed by : <a href="mailto:crecendia@hotmail.com">Putri Dewi P.</a><br><br><br>
+Photo taken by : <a href="mailto:bariqididan@gmail.com">Didan Bariqi</a><br><br><br>
+<br>
+<a href="http://github.com/nerdv2/sebelas_id_platform">GitHub link : http://github.com/nerdv2/sebelas_id_platform</a>
+<br>
+<article>
+<h3>App License</h3>
+<p>MIT License<br><br>
+
+Copyright (c) 2016 Gema Aji Wardian<br><br>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:<br><br>
+
+The above copyright notice and this permission notice <s></s>hall be included in all
+copies or substantial portions of the Software.<br><br>
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.</p>
+</article>
 </body>
 </html>
